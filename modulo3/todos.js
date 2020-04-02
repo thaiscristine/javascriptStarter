34493a3dd1ca0967c1f4529d381009b5874909e0
Thais Cristine <this.cristine06@gmail.com>
2020-04-02 15:35:36 -0300

    - add modulo 04

---
+++ modulo3/todos.js
@@ -0,0 +1,12 @@
+var list = document.querySelector('#app ul')
+var btnAdd = document.querySelector('#app button')
+var input = document.querySelector('#app input')
+
+var todos = [
+    'Test application',
+    'Buy tickets',
+    'meet at 17',
+];
+
+
+
