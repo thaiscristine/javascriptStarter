43ddee5b079c2bc8acc2443d36531f66e6cf8a0e
Thais Cristine <this.cristine06@gmail.com>
2020-04-02 16:26:12 -0300

    functions add and delete todo

--- modulo3/todos.js
+++ modulo3/todos.js
@@ -6,7 +6,43 @@ var todos = [
     'Test application',
     'Buy tickets',
     'meet at 17',
-];
+]
 
+function renderTodos() {
 
+    list.innerHTML = ''
+    for(todo of todos){
+        var listElement = document.createElement('li')
+        var listTextElement = document.createTextNode(todo)
 
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        var position = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTask(' + position + ')')
+
+        var linkTextElelemnt = document.createTextNode('Excluir')
+        linkElement.appendChild(linkTextElelemnt)
+
+        listElement.appendChild(listTextElement)
+        listElement.appendChild(linkElement)
+        list.appendChild(listElement)
+    }
+}
+
+renderTodos()
+
+function deleteTask(position){
+    todos.splice(position, 1)
+    renderTodos()
+}
+
+function addTask(){
+    var textTask = input.value
+
+    todos.push(textTask)
+    input.value = ''
+    renderTodos()
+}
+
+btnAdd.onclick = addTask
