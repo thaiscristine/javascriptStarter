8476e742a5a006f9dd3aa8c4d6f49e0f1805053f
Thais Cristine <this.cristine06@gmail.com>
2020-04-07 10:13:11 -0300

    challenge 02 correction

--- modulo4/challenge02.js
+++ modulo4/challenge02.js
@@ -8,13 +8,15 @@ btn.addEventListener('click', function(){
         .then(function(response) {
 
             var repositories = response.data
+            var lista = document.querySelector('#app ul')
+            lista.innerHTML = '';
 
             for(repository of repositories){
                 var listLiElement = document.createElement('li'); 
                 var textElement = document.createTextNode(repository.name); 
                 listLiElement.appendChild(textElement)
 
-                var lista = document.querySelector('#app ul')
+                
                 lista.appendChild(listLiElement)
 
                 // link => html_url   
